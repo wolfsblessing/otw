1a9fb8f0e3ea90c913584c402bfda4d7b9690579
bekim <wolfsblessing@gmail.com>
2014-07-09 16:49:33 +0200

    made the change so it starts with the index file

--- main.cpp
+++ main.cpp
@@ -8,8 +8,8 @@ int main(int argc, char *argv[])
     Html5ApplicationViewer viewer;
     viewer.setOrientation(Html5ApplicationViewer::ScreenOrientationAuto);
     viewer.showExpanded();
-//    viewer.loadFile(QLatin1String("../html/index.html"));
-    viewer.loadFile(QLatin1String("../html/closet.html"));
+    viewer.loadFile(QLatin1String("../html/index.html"));
+//    viewer.loadFile(QLatin1String("../html/closet.html"));
 
     viewer.setGeometry( 50, 50, 1000, 800 );
 
