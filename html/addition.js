4ef3ac85040660107337e503de1cb2b56536002e
bekim <wolfsblessing@gmail.com>
2014-08-14 15:12:24 +0200

    changed the layout of the add and subtractions pages. Colors and positions.

--- html/addition.js
+++ html/addition.js
@@ -31,12 +31,22 @@
             $("<div>" + k + "</div>").attr("id", "number" + k).appendTo("#choseNumbers");
             $("<div>" + "</div>").attr("id", "toShowFirst" + k).appendTo("#showFirst");
             $("<div>" + "</div>").attr("id", "toShowSecond" + k).appendTo("#showSecond");
+            $("<div>" + "</div>").attr("id", "toShowResult" + k).appendTo("#result_2");
         }
 
         for (k = 1; k <= 10; k++) {
             $("#toShowFirst"+ k).hide();
             $("#toShowSecond"+ k).hide();
+            $("#toShowResult"+ k).hide();
         }
+
+        $("#showFirst").hide();
+        $("#the_Plus_2").hide();
+        $("#showSecond").hide();
+        $("#the_equal_2").hide();
+        $("#result_2").hide();
+
+
 
         $("<div>" + "</div>").attr("id", "inResult").appendTo("#result");
 
@@ -53,24 +63,36 @@
                 stop: function() {
                     guessResult = guessResult + 1;
                     if (guessResult == 2) {
+                        $("#showFirst").fadeIn();
+                        $("#the_Plus_2").fadeIn();
+                        $("#showSecond").fadeIn();
+                        $("#the_equal_2").fadeIn();
+                        $("#result_2").fadeIn();
                 //creating the objects for the first number
-                    for (m = 1; m <= firstNumber[0]; m++) {
-                        $("#toShowFirst" + m).show();
-                    };
-                //creating the objects for the second number
-                    for (k = 1; k <= thirdNumber[0]; k++) {
-                        $("#toShowSecond" + k).show();
-                    };
+                        for (m = 1; m <= firstNumber[0]; m++) {
+                            $("#toShowFirst" + m).show();
+                        };
+                    //creating the objects for the second number
+                        for (k = 1; k <= thirdNumber[0]; k++) {
+                            $("#toShowSecond" + k).show();
+                        };
                         $("#number" + the_result).animate({
                             "background-color": "red"
                         }, 2000);
                     };
                     if (guessResult == 3) {
+                        $("#wholeRoom").animate({
+                            "width":"1000px"
+                        });
                         $("#inResult").show().text(the_result);
                         $("#result").droppable("disable");
-                        $("#choseNumbers").fadeOut(500);
+//                        $("#choseNumbers").fadeOut(500);
                         $("#popUp").fadeIn(3000);
                         $("#score").fadeIn(3000);
+
+                        for (m = 0; m <= the_result; m++) {
+                            $("#toShowResult" + m).show();
+                        };
                     }
                 }
             });
@@ -79,8 +101,24 @@
                 $("#result").droppable({
                     accept: "#number" + k,
                     drop: function(ev, ui) {
+                //creating the objects for the first number
+                        for (m = 1; m <= firstNumber[0]; m++) {
+                            $("#toShowFirst" + m).show();
+                        };
+                    //creating the objects for the second number
+                        for (k = 1; k <= thirdNumber[0]; k++) {
+                            $("#toShowSecond" + k).show();
+                        };
+                        $("#showFirst").fadeIn();
+                        $("#the_Plus_2").fadeIn();
+                        $("#showSecond").fadeIn();
+                        $("#the_equal_2").fadeIn();
+                        $("#result_2").fadeIn();
+                        for (m = 0; m <= the_result; m++) {
+                            $("#toShowResult" + m).show();
+                        };
                         $(this).droppable('disable');
-                        $("#choseNumbers").fadeOut(500);
+//                        $("#choseNumbers").fadeOut(500);
                         $("#inResult").show().text(the_result);
                         trueOrfalse = true;
                         ui.draggable.addClass("correct");
@@ -93,6 +131,9 @@
                         }).css({
                             "background-color": "red"
                         }).hide();
+                        $("#wholeRoom").animate({
+                            "width":"1000px"
+                        });
                         $("#popUp").fadeIn(3000);
                         $("#score").fadeIn(3000);
                     }
--- html/subtraction.js
+++ html/subtraction.js
@@ -28,7 +28,22 @@
 
         for (k = 0; k <= 10; k++) {
             $("<div>" + k + "</div>").attr("id", "number" + k).appendTo("#choseNumbers");
+            $("<div>" + "</div>").attr("id", "toShowFirst" + k).appendTo("#showFirst");
+            $("<div>" + "</div>").attr("id", "toShowSecond" + k).appendTo("#showSecond");
+            $("<div>" + "</div>").attr("id", "toShowResult" + k).appendTo("#result_2");
         }
+
+        for (k = 1; k <= 10; k++) {
+            $("#toShowFirst"+ k).hide();
+            $("#toShowSecond"+ k).hide();
+            $("#toShowResult"+ k).hide();
+        }
+
+        $("#showFirst").hide();
+        $("#the_Plus_2").hide();
+        $("#showSecond").hide();
+        $("#the_equal_2").hide();
+        $("#result_2").hide();
 
         $("<div>" + "</div>").attr("id", "inResult").appendTo("#result");
 
@@ -45,16 +60,35 @@
                 stop: function() {
                     guessResult = guessResult + 1;
                     if (guessResult == 2) {
+                        $("#showFirst").fadeIn();
+                        $("#the_Plus_2").fadeIn();
+                        $("#showSecond").fadeIn();
+                        $("#the_equal_2").fadeIn();
+                        $("#result_2").fadeIn();
+                    //creating the objects for the first number
+                        for (m = 1; m <= firstNumber[0]; m++) {
+                            $("#toShowFirst" + m).show();
+                        };
+                        for (k = 1; k <= secondNumber[0]; k++) {
+                            $("#toShowSecond" + k).show();
+                        };
                         $("#number" + the_result).animate({
                             "background-color": "red"
                         }, 2000);
                     };
                     if (guessResult == 3) {
+                        $("#wholeRoom").animate({
+                            "width":"1000px"
+                        });
                         $("#inResult").show().text(the_result);
                         $("#result").droppable("disable");
-                        $("#choseNumbers").fadeOut(500);
+//                        $("#choseNumbers").fadeOut(500);
+//                        $("#toShowResult0").fadeIn(3000).text(the_result);
                         $("#popUp").fadeIn(3000);
                         $("#score").fadeIn(3000);
+                        for (m = 0; m <= the_result; m++) {
+                            $("#toShowResult" + m).show();
+                        };
                     }
                 }
             });
@@ -63,8 +97,23 @@
                 $("#result").droppable({
                     accept: "#number" + k,
                     drop: function(ev, ui) {
+                        $("#showFirst").fadeIn();
+                        $("#the_Plus_2").fadeIn();
+                        $("#showSecond").fadeIn();
+                        $("#the_equal_2").fadeIn();
+                        $("#result_2").fadeIn();
+                    //creating the objects for the first number
+                        for (m = 1; m <= firstNumber[0]; m++) {
+                            $("#toShowFirst" + m).show();
+                        };
+                        for (k = 1; k <= secondNumber[0]; k++) {
+                            $("#toShowSecond" + k).show();
+                        };
+                        for (m = 0; m <= the_result; m++) {
+                            $("#toShowResult" + m).show();
+                        };
                         $(this).droppable('disable');
-                        $("#choseNumbers").fadeOut(500);
+//                        $("#choseNumbers").fadeOut(500);
                         $("#inResult").show().text(the_result);
                         trueOrfalse = true;
                         ui.draggable.addClass("correct");
@@ -77,6 +126,10 @@
                         }).css({
                             "background-color": "red"
                         }).hide();
+                        $("#wholeRoom").animate({
+                            "width":"1000px"
+                        });
+//                        $("#toShowResult0").fadeIn(3000).text(the_result);
                         $("#popUp").fadeIn(3000);
                         $("#score").fadeIn(3000);
                     }
