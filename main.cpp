c399388f1f6db05e5a49bda45477f2a3527065c5
Denis Aleksandrov <denal05@gmail.com>
2014-09-12 11:58:54 +0200

    Changed the relative path of index.html as it is being loaded from inside main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,8 @@ int main(int argc, char *argv[])
     Html5ApplicationViewer viewer;
     viewer.setOrientation(Html5ApplicationViewer::ScreenOrientationAuto);
     viewer.showExpanded();
-    viewer.loadFile(QLatin1String("../html/index.html"));
+    viewer.loadFile(QLatin1String("html/index.html"));
+    //viewer.loadFile(QLatin1String("C:/proj/otw/html/index.html"));
     viewer.setGeometry( 50, 50, 1000, 800 );
 
     return app.exec();
