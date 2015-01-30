2df95f0dc41e85c29d79d8f1fd3dbcd5d683fc28
Bekim <wolfsblessing@gmail.com>
2015-01-30 16:59:33 +0100

    Added mk and sq audio files for index page. For now only welcome and chose language is added

--- html/index.js
+++ html/index.js
@@ -1,5 +1,34 @@
-﻿(function() {
+(function() {
+    
     function init() {
+        var audio, dir_mk_index, playlist_mk, playlist_sq, playlist_index, ext, audio_order;
+        
+        dir_mk_index = "mk_sounds/index/";
+        dir_sq_index = "sq_sounds/index/";
+        playlist_mk = ["welcome", "jazik", "makedonski", "shqip", "continue"];
+        playlist_sq = ["welcome_alb", "jazik_alb", "makedonski_alb", "shqip_alb", "continue_alb"];
+        audio_order = [dir_mk_index+"jazik", dir_sq_index+"jazik_alb", dir_mk_index+"makedonski", dir_sq_index+"shqip_alb"];
+        ext = ".ogg";
+        
+        playlist_index = 0;
+        
+        audio = new Audio();
+        
+        audio.src = audio_order[0] + ext;
+        audio.play();
+        audio.addEventListener("ended", function() { switch_me(); });
+        
+        function switch_me(){
+            if(playlist_index == (audio_order.length-1)){
+                audio.pause();
+            } else {
+                number_me();
+                audio.src = audio_order[playlist_index+1] + ext;
+                setTimeout(function(){audio.play();}, 2000);
+                playlist_index++;
+            }
+        }
+        
 
        $("#chose_mk_page").hide();
        $("#chose_sq_page").hide();
@@ -16,19 +45,27 @@
         $("#goToIndexMK").click(function() {
             $("#front_page").fadeIn(2000);
             $("#welcome").fadeIn(2000);
+            audio.src = dir_mk_index+"welcome"+ext;
+            audio.play();
+            function number_me(){
+                playlist_index = audio_order.length-1;
+            }
 
             $("#front_page_sq").hide();
             $("#welcome_sq").hide();
         });
+        
+        
 
         $("#goToIndexSQ").click(function() {
             $("#front_page_sq").fadeIn(2000);
             $("#welcome_sq").fadeIn(2000);
+            audio.src = dir_sq_index+"welcome_alb"+ext;
+            audio.play();
 
             $("#front_page").hide();
             $("#welcome").hide();
         });
-
 
     }
     
