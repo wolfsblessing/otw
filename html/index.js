63ddada203e8fb4a0e04c851d86e90e4b945684c
Bekim <wolfsblessing@gmail.com>
2015-02-02 11:27:41 +0100

    Added the secondary files that should be triggered by clicking on a button that will chose the language.

--- html/index.js
+++ html/index.js
@@ -1,7 +1,8 @@
 (function() {
     
     function init() {
-        var audio, dir_mk_index, playlist_mk, playlist_sq, playlist_index, ext, audio_order;
+        var audio, dir_mk_index, playlist_mk, playlist_sq, playlist_index, ext, audio_order, stop_sound, stop_continue;
+        
         
         dir_mk_index = "mk_sounds/index/";
         dir_sq_index = "sq_sounds/index/";
@@ -10,7 +11,12 @@
         audio_order = [dir_mk_index+"jazik", dir_sq_index+"jazik_alb", dir_mk_index+"makedonski", dir_sq_index+"shqip_alb"];
         ext = ".ogg";
         
+        continue_mk = audio_order[0]+ext;
+        
         playlist_index = 0;
+        
+        stop_sound = false;
+        stop_continue = false;
         
         audio = new Audio();
         
@@ -21,8 +27,11 @@
         function switch_me(){
             if(playlist_index == (audio_order.length-1)){
                 audio.pause();
-            } else {
                 number_me();
+            } else {
+                if(stop_sound == true){
+                    number_me();
+                }
                 audio.src = audio_order[playlist_index+1] + ext;
                 setTimeout(function(){audio.play();}, 2000);
                 playlist_index++;
@@ -43,13 +52,25 @@
        $("#chose_sq_page").fadeIn(3000);
 
         $("#goToIndexMK").click(function() {
+            stop_sound = true;
             $("#front_page").fadeIn(2000);
             $("#welcome").fadeIn(2000);
             audio.src = dir_mk_index+"welcome"+ext;
             audio.play();
             function number_me(){
                 playlist_index = audio_order.length-1;
-            }
+            };
+            audio.addEventListener("ended", function(){stop_continue(); });
+            
+            function stop_continue(){
+                if (stop_continue == true){
+                    audio.pause();
+                }else{
+                    audio.src = dir_mk_index+"continue"+ext;
+                    setTimeout(function(){audio.play();}, 2000);
+                    stop_continue = true;
+                }
+            };
 
             $("#front_page_sq").hide();
             $("#welcome_sq").hide();
@@ -58,10 +79,26 @@
         
 
         $("#goToIndexSQ").click(function() {
+            stop_sound = true;
             $("#front_page_sq").fadeIn(2000);
             $("#welcome_sq").fadeIn(2000);
             audio.src = dir_sq_index+"welcome_alb"+ext;
             audio.play();
+            
+            function number_me(){
+                playlist_index = audio_order.length-1;
+            };
+            audio.addEventListener("ended", function(){stop_continue(); });
+            
+            function stop_continue(){
+                if (stop_continue == true){
+                    audio.pause();
+                }else{
+                    audio.src = dir_sq_index+"continue_alb"+ext;
+                    setTimeout(function(){audio.play();}, 2000);
+                    stop_continue = true;
+                }
+            };
 
             $("#front_page").hide();
             $("#welcome").hide();
